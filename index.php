7dc8a1d45b3f40dbc691568757faf5966355a8a8
UseYourFist <spr47@mail.ru>
2016-03-18 16:22:40 +0600

    new project

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Сегодняшняя дата</title>
+</head>
+<body>
+<p>Сегодняшняя дата(согласно этому веб-серверу):
+
+    <?php
+    echo date('l, F jS Y.');
+    ?>
+</p>
+</body>
+</html>
